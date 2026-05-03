971b0cdfc1a2f945b1e4072d17c0c825b14e01ee
haru1489248 <kyudo.9248@icloud.com>
2026-05-03 14:06:48 +0900

    complete ch06

---
+++ ch05/simple_rnnlm.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.time_layers import *
+
+class SimpleRnnlm:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        """
+        rnnとaffineにはXavierの初期値を使用している。Xavierの初期値とは標準偏差が 1 / \sqrt{D} になるように重みを初期化する手法である。出力が偏りすぎるとシグモイド関数やtanh関数で出力が偏り、勾配消失や勾配爆発が起きる可能性がある。
+        Xavierの初期値はsigmoid関数・tanh関数に適している。
+        """
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        # 重みの初期化
+        embed_W = (rn(V, D) / 100).astype('f')
+        rnn_Wx = (rn(D, H) / np.sqrt(D)).astype('f')
+        rnn_Wh = (rn(H, H) / np.sqrt(H)).astype('f')
+        rnn_b = np.zeros(H).astype('f')
+        affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+
+        # レイヤの生成
+        self.layers = [
+            TimeEmbedding(embed_W),
+            TimeRNN(rnn_Wx, rnn_Wh, rnn_b, stateful=True),
+            TimeAffine(affine_W, affine_b)
+        ]
+        self.loss_layer = TimeSoftmaxWithLoss()
+        self.rnn_layer  = self.layers[1]
+
+        # 全ての重みと勾配をリストにまとめる
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, xs, ts):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        loss = self.loss_layer.forward(xs, ts)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+
+    def reset_state(self):
+        self.rnn_layer.reset_state()
---
+++ ch07/rnnlm_gen.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.functions import softmax
+from ch06.rnnlm import Rnnlm
+from ch06.better_rnnlm import BetterRnnlm
+
+class RnnlmGen(Rnnlm):
+    def generate(self, start_id, skip_ids=None, sample_size=100):
+        """
+        start_id: 最初に与える単語ID
+        sample_size: サンプリングする単語数
+        skip_ids: サンプリングして欲しくない単語IDリスト
+        """
+        word_ids = [start_id]
+
+        x = start_id
+        while len(word_ids) < sample_size:
+            x = np.array(x).reshape(1, 1) # ミニバッチ想定で実装されているpredictに合わせて2次元にする
+            score = self.predict(x)
+            p = softmax(score.flatten())
+
+            sampled = np.random.choice(len(p), size=1, p=p)
+            print("sampled", sampled)
+
+            if (skip_ids is None) or (sampled not in skip_ids):
+                x = sampled
+                word_ids.append(int(x))
+
+        return word_ids
