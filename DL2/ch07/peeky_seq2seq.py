36e85a8ba548bfca60453ec1abc4f2dad40fb9ff
haru1489248 <kyudo.9248@icloud.com>
2026-05-04 15:15:12 +0900

    complete ch07

---
+++ DL2/ch07/peeky_seq2seq.py
@@ -0,0 +1,91 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.time_layers import *
+from ch07.seq2seq import Encoder, Seq2Seq
+
+class PeekyDecoder:
+    """
+    peek = 覗くからきている。Encoderが作成した隠れ状態ベクトルをDecoderの複数の層に共有する。
+    """
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(H + D, 4 * H) / np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        affine_W = (rn(H + H, V) / np.sqrt(H + H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx ,lstm_Wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_W, affine_b)
+
+        self.params, self.grads = [], []
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+        self.cache = None
+
+    def forward(self, xs, h):
+        N, T = xs.shape
+        N, H = h.shape
+
+        self.lstm.set_state(h)
+
+        out = self.embed.forward(xs) # out shape (N, T, D)
+        hs = np.repeat(h, T, axis=0).reshape(N, T, H) # hs shape (N, T, H)
+        out = np.concatenate((hs, out), axis=2) # out shape (N, T, H + D)
+
+        out = self.lstm.forward(out)
+        out = np.concatenate((hs, out), axis=2)
+
+        score = self.affine.forward(out)
+        self.cache = H
+        return score
+
+    def backward(self, dscore):
+        H = self.cache
+
+        dout = self.affine.backward(dscore)
+        dout, dhs0 = dout[:, :, H:], dout[:, :, :H] # lstm側の勾配とencoder側の勾配を分割
+        dout = self.lstm.backward(dout)
+        dembed, dhs1 = dout[:, :, H:], dout[:, :, :H] # embedding側の勾配とencoder側の勾配を分割
+        self.embed.backward(dembed)
+
+        dhs = dhs0 + dhs1
+        dh = self.lstm.dh + np.sum(dhs, axis=1)
+        return dh
+
+    def generate(self, h, start_id, sample_size):
+        sampled = []
+        char_id = start_id
+        self.lstm.set_state(h)
+
+        H = h.shape[1]
+        peeky_h = h.reshape(1, 1, H)
+        for _ in range(sample_size):
+            x = np.array([char_id]).reshape((1, 1))
+            out = self.embed.forward(x)
+
+            out = np.concatenate((peeky_h, out), axis=2)
+            out = self.lstm.forward(out)
+            out = np.concatenate((peeky_h, out), axis=2)
+            score = self.affine.forward(out)
+
+            char_id = np.argmax(score.flatten())
+            sampled.append(char_id)
+
+        return sampled
+
+class PeekySeq2Seq(Seq2Seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = PeekyDecoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
--- DL2/ch07/rnnlm_gen.py
+++ DL2/ch07/rnnlm_gen.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('..')
 import numpy as np
-from DL2.common.functions import softmax
-from DL2.ch06.rnnlm import Rnnlm
-from DL2.ch06.better_rnnlm import BetterRnnlm
+from common.functions import softmax
+from ch06.rnnlm import Rnnlm
+from ch06.better_rnnlm import BetterRnnlm
 
 class RnnlmGen(Rnnlm):
     def generate(self, start_id, skip_ids=None, sample_size=100):
@@ -27,3 +27,34 @@ class RnnlmGen(Rnnlm):
                 word_ids.append(int(x))
 
         return word_ids
+
+    def get_state(self):
+        self.lstm_layer.h, self.lstm_layer.c
+
+    def set_state(self, state):
+        self.lstm_layer.set_state(*state)
+
+class BetterRnnlmGen(BetterRnnlm):
+    def generate(self, start_id, skip_ids=None, sample_size=100):
+        word_ids = [start_id]
+
+        x = start_id
+        while len(word_ids) < sample_size:
+            x = np.array(x).reshape(1, 1) # ミニバッチ想定で実装されているpredictに合わせて2次元にする
+            score = self.predict(x)
+            p = softmax(score.flatten())
+
+            sampled = np.random.choice(len(p), p=p)
+
+            if (skip_ids is None) or (sampled not in skip_ids):
+                x = sampled
+                word_ids.append(int(x))
+
+        return word_ids
+
+    def get_state(self):
+        self.lstm_layer.h, self.lstm_layer.c
+
+    def set_state(self, state):
+        self.lstm_layer.set_state(*state)
+
